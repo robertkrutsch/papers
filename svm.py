3861a123f5a4312ba9c28079440ffda85ec41be3
robert <krutsch@gmail.com>
2018-09-20 19:46:35 +0200

    added training and validation on the training dataset

--- svm.py
+++ svm.py
@@ -13,29 +13,24 @@ from sqlite3 import dbapi2 as sqlite3
 # local imports
 from utils import Config
 
+"""
+This compares the output of the classifier with the reference.
+"""
+def get_performance(out,ref):
+    false_positive = 0
+    false_negative = 0
+    for i in range(len(out)):
+        if (ref[i] == 1) and (out[i] < 0):
+            false_negative = false_negative + 1
+        if (ref[i] == 0) and (out[i] > 0):
+            false_positive = false_positive + 1
 
-def get_filepaths(directory):
-    """
-    This function will generate the file names in a directory
-    tree by walking the tree either top-down or bottom-up. For each
-    directory in the tree rooted at directory top (including top itself),
-    it yields a 3-tuple (dirpath, dirnames, filenames).
-    Assumption: the files do not have some .png in their name
-    """
-    file_paths = []  # List which will store all of the full filepaths.
+    return false_negative,false_positive
 
-    # Walk the tree.
-    for root, directories, files in os.walk(directory):
-        for filename in files:
-            # Join the two strings in order to form the full filepath.
-            if filename.find('.pdf.txt') > 0 : # get only the pdf.txt files
-              filepath = os.path.join(root, filename)
-              file_paths.append(filepath)  # Add it to the list.
-
-    return file_paths  # Self-explanatory.
-
+"""
+check if the database with features exist
+"""
 def check_file_esist(tfidf_path,filelist_path):
-    #check if the database with features exist
     if not os.path.isfile(tfidf_path):
         print("You need to run tfidf first to produce the features!")
         sys.exit()
@@ -44,6 +39,7 @@ def check_file_esist(tfidf_path,filelist_path):
         sys.exit()
 
 len_usr = []
+len_usr.append(0) #first needs to be zero such that we loop properly
 for i in range(1, Config.nr_users + 1, 1):
     usr_dir = Config.dataset_dir + "train/usr" + str(i)
     tfidf_path = usr_dir + Config.tfidf_path
@@ -56,25 +52,25 @@ for i in range(1, Config.nr_users + 1, 1):
     out = pickle.load(open(tfidf_path, 'rb'))
     tmp = out['X']
     tmp = tmp.todense()
-    len_usr.append(tmp.shape[0])
-    X = np.concatenate((tmp, tmp), axis=0)
+    len_usr.append(tmp.shape[0]) # keep the number of the files that we have for each user
+    if (i==1):
+        X = tmp
+    else:
+        X = np.concatenate((X, tmp), axis=0)
 
+poz = 0
 for i in range(1, Config.nr_users + 1, 1):
     y = np.zeros(X.shape[0])
+    poz = poz + len_usr[i-1]
+    for ix in range(len_usr[i]):
+        y[poz + ix] = 1
 
+    clf = svm.LinearSVC(class_weight='balanced', verbose=False, max_iter=10000, tol=1e-6, C=0.1)
+    clf.fit(X, y)
 
+    s = clf.decision_function(X) #evaluate on the training dataset
 
-#TBD Need to split to the users and then assigns 0 for the wrong paper and 1 for the right paper
-#TBD might need to change the tfidf to be for each user and to be for training and validation different...
-"""
-y = np.zeros(X.shape[0])
-for ix in posix: y[ix] = 1
+    false_negative, false_positive = get_performance(s, y)
+    print("Statistic on usr"+str(i)+":",false_negative,false_positive)
 
-clf = svm.LinearSVC(class_weight='balanced', verbose=False, max_iter=10000, tol=1e-6, C=0.1)
-clf.fit(X, y)
-s = clf.decision_function(X)
-
-sortix = np.argsort(-s)
-sortix = sortix[:min(num_recommendations, len(sortix))]  # crop paper recommendations to save space
-user_sim[uid] = [strip_version(meta['pids'][ix]) for ix in list(sortix)]
-"""
+    #TBD - need to compare to the validation set and also against the trainingset, some comparing function would be good to have
